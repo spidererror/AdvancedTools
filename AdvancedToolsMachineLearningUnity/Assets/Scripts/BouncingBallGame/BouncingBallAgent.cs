a77cbbe0e0f599b871b9481d4ef2566aa9cf10bc
Niels Prins <447730@student.saxion.nl>
2021-03-15 01:37:51 +0100

    Adding bouncing ball scene with basic setup

---
+++ AdvancedToolsMachineLearningUnity/Assets/Scripts/BouncingBallGame/BouncingBallAgent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+public class BouncingBallAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
