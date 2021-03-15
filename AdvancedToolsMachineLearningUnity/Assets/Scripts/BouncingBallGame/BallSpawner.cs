345d350c9ccb81587598485f5729dc9ea995619e
Niels Prins <447730@student.saxion.nl>
2021-03-15 22:32:32 +0100

    BallSpawner script can spawns objects in a random radius.

--- AdvancedToolsMachineLearningUnity/Assets/Scripts/BouncingBallGame/BallSpawner.cs
+++ AdvancedToolsMachineLearningUnity/Assets/Scripts/BouncingBallGame/BallSpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class BallSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int _maxAmountOfBalls = 30;
+    [SerializeField]
+    private int _radius = 30;
+    [SerializeField]
+    private List<GameObject> _differentBalls = new List<GameObject>();
+    private List<GameObject> _currentActiveBalls = new List<GameObject>();
+
+    private void Start()
     {
-        
+        RandomSpawnLocation();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void RandomSpawnLocation()
     {
-        
+        while (_currentActiveBalls.Count < _maxAmountOfBalls)
+        {
+            _currentActiveBalls.Add(Instantiate(_differentBalls[Random.Range(0,_differentBalls.Count)],transform));
+        }
+        foreach (GameObject balls in _currentActiveBalls)
+        {
+            Vector3 randomPosition = transform.position + new Vector3(Random.Range(-_radius,_radius),0,Random.Range(-_radius,_radius));
+            balls.transform.position = randomPosition;
+        }
     }
 }
