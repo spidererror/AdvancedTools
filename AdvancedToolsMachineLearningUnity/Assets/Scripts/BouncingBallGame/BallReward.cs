3db85e67cb1e51ed10111f32016a7569497938b9
Niels Prins <447730@student.saxion.nl>
2021-03-15 23:16:10 +0100

    Added Ball reward script where the reward of each ball can be set.

--- AdvancedToolsMachineLearningUnity/Assets/Scripts/BouncingBallGame/BallReward.cs
+++ AdvancedToolsMachineLearningUnity/Assets/Scripts/BouncingBallGame/BallReward.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using System.Linq;
 
 public class BallReward : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int _currentReward;
+
+    private BouncingBallAgent _agent;
+
+    private void Start()
     {
-        
+        _agent = GameObject.FindGameObjectsWithTag("Player").First().GetComponent<BouncingBallAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddReward()
     {
-        
+        _agent.AddReward(_currentReward);
     }
 }
